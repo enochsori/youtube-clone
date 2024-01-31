88470c637888b2354d4c448a5807ee6514164d40
Enoch <enoch@cydef.ca>
2024-01-31 15:33:26 -0500

    react query and axios

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,11 +1,38 @@
+import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
+import { FaSpinner } from 'react-icons/fa6';
+import { BiError } from 'react-icons/bi';
+import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
 
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery({
+    queryKey: ['videos', keyword],
+    queryFn: async () => {
+      return axios
+        .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+        .then((res) => res.data.items);
+    },
+  });
+
   return (
     <>
       <div className='text-lg'>Videos : {keyword ? keyword : 'hot videos'}</div>
+      {isLoading && <FaSpinner className='text-4xl' />}
+      {error && <BiError />}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
     </>
   );
 }
